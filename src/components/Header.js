601118f37ae8230c1962d4b9ac110d7f9408a458
Shristi Gupta <gshristi40@gmail.com>
2024-07-25 23:32:27 +0530

    responsive header ready

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Header = () => {
   return (
-    <header className="pl-12 text-white text-2xl bg-blue-1000 w-full h-16 flex items-center">
+    <header className="md:pl-12 text-white text-2xl bg-blue-1000 w-full h-16 flex justify-center md:justify-start items-center">
       <span>Wellness Retreats</span>
     </header>
   );

61c1cf275c3578d6192991cdc3ed6fa9489d1fe8
Shristi Gupta <gshristi40@gmail.com>
2024-07-26 23:28:17 +0530

    added select and search inputs

--- src/components/Body.js
+++ src/components/Body.js
@@ -12,7 +12,24 @@ export const Body = () => {
         }
       />
 
-      <div></div>
+      <div className="flex justify-between">
+        <div className="flex gap-4">
+          <div className=" bg-blue-1000 py-2 px-2 rounded-md">
+            <label className="text-white text-sm">Filter by Date</label>
+            <select className="bg-transparent">Filter by date</select>
+          </div>
+          <div className=" bg-blue-1000 py-2 px-2 rounded-md">
+            <label className="text-white text-sm">Filter by Type</label>
+            <select className="bg-transparent"></select>
+          </div>
+        </div>
+        <div className="w-[25rem]">
+          <input
+            className="bg-blue-1000 text-white w-full focus:outline-none placeholder:text-white p-2 rounded-md  placeholder:text-sm"
+            placeholder="Search retreats by title"
+          />
+        </div>
+      </div>
 
       <div className="flex gap-4 w-full">
         <SmallCard />
